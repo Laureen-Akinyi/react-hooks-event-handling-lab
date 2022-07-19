fbe9662c1a4fd39691761b0d232aa2bb343d3a28
lorain <laureen.akinyi@student.moringaschool.com>
2022-07-20 01:02:54 +0300

    commit tree 7e3deb226826a9aad42e01550d5e46c8aff339f6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+
+function Keypad(){
+    function nuclearChange(){
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+            <input type="password" onChange={nuclearChange} />
+        
     )
 }
 
